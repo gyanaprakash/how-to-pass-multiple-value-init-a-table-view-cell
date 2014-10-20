395e8e19a572091a463ef4b27b868403de6464f1
gyana prakash gouda <gyanaprakashgouda471@gmail.com>
2014-10-20 18:29:08 +0530

    how to pass multiple value init a table view cell
    how to pass multiple value init a table view cell
    their are two table view one is name and other is age. i want to pass
    both value in one cell multiple times.

--- task.3/ViewController.h
+++ task.3/ViewController.h
@@ -8,8 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate,UIPickerViewDelegate,UIPickerViewDataSource,UITableViewDataSource,UITableViewDelegate>
+{
+    NSArray *Agepicker;
+    UITableView *dataTable;
+    NSMutableArray *textarray;
+    
+}
+@property (nonatomic, retain) NSMutableArray *textarray;
+@property (nonatomic, retain) UITableView *dataTable;
+@property (nonatomic, retain) NSArray *Agepicker;
+@property (weak, nonatomic) IBOutlet UITextField *txtFldName;
+@property (weak, nonatomic) IBOutlet UITextField *txtFldAge;
+@property (strong, nonatomic) IBOutlet UIPickerView *pickerAge;
+@property (weak, nonatomic) IBOutlet UIButton *savebutton;
 
+- (IBAction)savebutton:(id)sender;
+- (IBAction)hideKeyBoard:(id)sender;
 
 @end
 
--- task.3/ViewController.m
+++ task.3/ViewController.m
@@ -14,14 +14,123 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+@synthesize txtFldAge,txtFldName,pickerAge,Agepicker,dataTable,textarray;
+
+- (void)viewDidLoad
+{
+    
+    dataTable=[[UITableView alloc]init];
+    [dataTable setDataSource:self];
+    [dataTable setDelegate:self];
+    [dataTable setHidden:YES];
+    [dataTable setFrame:CGRectMake(0, 300, 370, 150)];
+    [self.view addSubview:dataTable];
+    
+    textarray=[[NSMutableArray alloc]init];
+
+    txtFldAge.inputView = pickerAge;
+    
+    Agepicker=[NSArray arrayWithObjects:@"19",@"20",@"21",@"22",@"23",@"24",@"25",@"26",@"27",@"28",@"29",@"30",@"19",@"20",@"21",@"22",@"23",@"24",@"25",@"26",@"27",@"28",@"29",@"30",@"19",@"20",@"21",@"22",@"23",@"24",@"25",@"26",@"27",@"28",@"29",@"30", nil];
+    [pickerAge setFrame:CGRectMake(207, 50, 97, 50)];
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (IBAction)hideKeyBoard:(id)sender
+{
+    [txtFldAge resignFirstResponder];
+    [txtFldName resignFirstResponder];
+
+}
+
+
+- (void)didReceiveMemoryWarning
+
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)savebutton:(id)sender
+{
+    if (txtFldName.text.length==0 )
+    {
+        UIAlertView *alrt=[[UIAlertView alloc]initWithTitle:@"Warning !" message:@"Enter the Name:" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alrt show];
+    }
+    else if (txtFldAge.text.length==0)
+    {
+        UIAlertView *alrt=[[UIAlertView alloc]initWithTitle:@"Warning !" message:@"Enter the Age:" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alrt show];
+    }
+    else
+    {
+        NSMutableDictionary *detail=[[NSMutableDictionary alloc]init];
+        [detail setObject:txtFldName.text forKey:@"Name"];
+        [detail setObject:txtFldAge.text forKey:@"Age"];
+        
+        [textarray addObject:detail];
+        
+        [dataTable setHidden:NO];
+        [dataTable reloadData];
+    }
+}
+
+#pragma mark - UIPicker view datasource methods
+
+// returns the number of 'columns' to display.
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+
+{
+    return 1;
+}
+
+// returns the # of rows in each component..
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+
+{
+    return Agepicker.count;
+}
+
+#pragma mark - UIPicker view delegate methods
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+
+{
+    return Agepicker[row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+
+{
+    txtFldAge.text=Agepicker[row];
+    [txtFldAge resignFirstResponder];
+}
+
+#pragma mark uitableview data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+
+{
+    return [textarray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+
+{
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"cell1"];
+    if (cell==nil)
+    {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell1"];
+    }
+    
+   cell.textLabel.text= [NSString stringWithFormat:@"Name : %@",[[textarray objectAtIndex:indexPath.row]  valueForKey:@"Name"]];
+   cell.detailTextLabel.text= [NSString stringWithFormat:@"Age : %@",[[textarray objectAtIndex:indexPath.row]  valueForKey:@"Age"]];
+
+    return cell;
 }
 
 @end
