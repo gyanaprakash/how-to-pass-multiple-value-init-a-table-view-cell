7895bcf04732347edf4593b7f9fcf141458673dd
gyana prakash gouda <gyanaprakashgouda471@gmail.com>
2014-10-20 13:29:46 +0530

    Initial Commit

---
+++ task.3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  task.3
+//
+//  Created by Bsetecip10 on 20/10/14.
+//  Copyright (c) 2014 Bsetecip10. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
